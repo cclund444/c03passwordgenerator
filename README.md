9bc0f417d10ef6e272aeb3fe89631fe7d67cc4e1
cclund444 <87826114+cclund444@users.noreply.github.com>
2021-09-04 21:28:52 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# c03passwordgenerator
-I created varables with text input amounts/options.
-I created prompts to have user select choices for varriable (Yes.No)
-Implamented a randamized system to generate password
+# Password Generator Starter Code
---
+++ script.js
@@ -0,0 +1,73 @@
+// Assignment code here
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var number = "123456789"
+var symbol = "!@#$%^&*();"
+
+var master = "";
+var random = []
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+  // Add event listener to generate button;
+  // generateBtn.addEventListener("click", userPrompts);
+
+// Write password to the #password input
+function askLength() {
+  var length = prompt("How Many Characters?");
+  return length
+}
+
+function charChoice() {
+  
+  var wantLower = confirm("Would you like lowercase letters?")
+  var wantUpper = confirm("Would you like uppercase letters?")
+  var wantNumber = confirm("Would you like numbers used?")
+  var wantSymbol = confirm("Would you like symbols used?")
+  
+  if (wantLower == true) {
+    master += lowerCase
+  }
+  if (wantUpper == true) {
+    master += upperCase
+  }
+  if (wantNumber == true) {
+    master += number
+  }
+  if (wantSymbol == true) {
+    master += symbol
+  }
+
+}
+
+function userPrompts() {
+  var lengthAns = askLength()
+  if (lengthAns < 8 || lengthAns > 128) {
+    alert("Please enter a number between 8 and 128.")
+    lengthAns = askLength()
+  }
+
+  charChoice()
+  if (master === "") {
+    alert("Please select at least one character type.")
+    charChoice()
+  }
+
+  writePassword(lengthAns)
+
+}
+
+function writePassword(length) {
+  // for loop that takes a random character from the master string and puts it onto a final string repeatedly until password length is met
+ for (let i = 0; i < length; i++) {
+   
+ }
+
+  // Put the password on the screen
+ 
+
+};
+
+// Add event listener to generate button
+ generateBtn.addEventListener("click", userPrompts);
